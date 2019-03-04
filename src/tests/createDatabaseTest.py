69149979b14414e1fbf18933ab8939cd4f2082d2
Dirk Hartmann <2monex@gmx.net>
2019-03-04 21:16:53 +0100

    Added test case for checking the update call

--- src/tests/createDatabaseTest.py
+++ src/tests/createDatabaseTest.py
@@ -59,6 +59,33 @@ class CreateDatabaseTestCase(unittest.TestCase):
     zmConfigMock.changeConfigValue.assert_called_once_with("ZM_DB_PASS", "testpass")
     zmConfigMock.writeConfigFile.assert_called_once()
     
+  
+  @patch("os.remove")
+  @patch("os.path.isfile")
+  @patch("zm_dbinit.mysql_command.MySQLCommand", autospec = True)
+  def testUpdateDatabaseisCalled(self, createDatabaseMock, isFileMock, fileRemoveMock):
+    with patch.object(DatabaseInit, "updateDatabase") as udpateDatabaseMock, \
+         patch.object(DatabaseInit, "getInstalledVersion") as getInstallaedVersionMock, \
+         patch.object(DatabaseInit, "checkLockFile") as checkLockFileMock, \
+         patch.object(DatabaseInit, "rootUserCheck") as rootUserCheckMock, \
+         patch.object(DatabaseInit, "getInstalledVersion") as getInstalledVersionMock, \
+         patch.object(DatabaseInit, "restartApache") as restartApacheMock:
+      getInstallaedVersionMock.return_value = "1.30.4"
+      checkLockFileMock.return_value = True
+      getInstalledVersionMock.return_value = "1.30.1"
+      isFileMock.return_value = True
+      
+      dbInit = DatabaseInit(self.prompt, self.config)
+      dbInit.mysql = createDatabaseMock
+      
+      #from pudb import set_trace; set_trace()
+      dbInit.initializeDatabase()
+      
+      isFileMock.assert_called_once_with(self.config.zmLockFile())
+      fileRemoveMock.assert_called_once_with(self.config.zmLockFile())
+      udpateDatabaseMock.assert_called_once_with("1.30.1", "1.30.4")
+      restartApacheMock.assert_called_once()
+    
     
 if __name__ == "__main__":
   unittest.main()
